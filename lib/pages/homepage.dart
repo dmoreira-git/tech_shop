e14eaf6c8309b9d7d8315e2c45c3b4bfe6ebc30e
Daniel Moreira <smitch17@gmail.com>
2025-05-08 16:48:44 -0300

    adding navigation bar to switch pages

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:tech_shop/components/bottom_nav_bar.dart';
+import 'cartpage.dart';
+import 'settingspage.dart';
+import 'shoppage.dart';
 
 class HomePage extends StatefulWidget{
   const HomePage({super.key});
@@ -8,10 +12,30 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage>{
+
+  int _selectedBottomBarIndex = 0;
+
+  void navigateBottomBar(int index){
+    setState(() {
+      _selectedBottomBarIndex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    const ShopPage(),
+    const CartPage(),
+    const SettingsPage(),
+
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
+      bottomNavigationBar: StoreBottomBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedBottomBarIndex],
     );
   }
 
